559f253137d8417835736b35c36ceec77c6b738c
PajarKharisma <pajarkharisma@hotmail.com>
2019-08-30 14:16:16 +0700

    after rollback

--- resources/views/template/backend.blade.php
+++ resources/views/template/backend.blade.php
@@ -8,16 +8,16 @@
     <title>Sistem Penilaian SD</title>
 
     <!-- Bootstrap 3.3.7 -->
-    {!! Html::style('adminlte/bower_components/bootstrap/dist/css/bootstrap.min.css') !!}
-    {!! Html::style('adminlte/bower_components/font-awesome/css/font-awesome.min.css') !!}
-    {!! Html::style('adminlte/bower_components/Ionicons/css/ionicons.min.css') !!}
-    {!! Html::style('adminlte/dist/css/AdminLTE.min.css') !!}
-    {!! Html::style('adminlte/dist/css/skins/_all-skins.min.css') !!}
-    {!! Html::style('adminlte/bower_components/morris.js/morris.css') !!}
-    {!! Html::style('adminlte/bower_components/jvectormap/jquery-jvectormap.css') !!}
-    {!! Html::style('adminlte/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css') !!}
-    {!! Html::style('adminlte/bower_components/bootstrap-daterangepicker/daterangepicker.css') !!}
-    {!! Html::style('adminlte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css') !!}
+    {!! Html::style('lte/bower_components/bootstrap/dist/css/bootstrap.min.css') !!}
+    {!! Html::style('lte/bower_components/font-awesome/css/font-awesome.min.css') !!}
+    {!! Html::style('lte/bower_components/Ionicons/css/ionicons.min.css') !!}
+    {!! Html::style('lte/dist/css/AdminLTE.min.css') !!}
+    {!! Html::style('lte/dist/css/skins/_all-skins.min.css') !!}
+    {!! Html::style('lte/bower_components/morris.js/morris.css') !!}
+    {!! Html::style('lte/bower_components/jvectormap/jquery-jvectormap.css') !!}
+    {!! Html::style('lte/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css') !!}
+    {!! Html::style('lte/bower_components/bootstrap-daterangepicker/daterangepicker.css') !!}
+    {!! Html::style('lte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css') !!}
     {!! Html::style('css/jquery-confirm.min.css') !!}
     <style>
     </style>
@@ -27,31 +27,31 @@
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 
-    {!! Html::script('adminlte/bower_components/jquery/dist/jquery.min.js') !!}
-    {!! Html::script('adminlte/bower_components/jquery-ui/jquery-ui.min.js') !!}
-    {!! Html::script('adminlte/bower_components/bootstrap/dist/js/bootstrap.min.js') !!}
+    {!! Html::script('lte/bower_components/jquery/dist/jquery.min.js') !!}
+    {!! Html::script('lte/bower_components/jquery-ui/jquery-ui.min.js') !!}
+    {!! Html::script('lte/bower_components/bootstrap/dist/js/bootstrap.min.js') !!}
     <!-- Sparkline -->
-    {!! Html::script('adminlte/bower_components/jquery-sparkline/dist/jquery.sparkline.min.js') !!}
+    {!! Html::script('lte/bower_components/jquery-sparkline/dist/jquery.sparkline.min.js') !!}
     <!-- jvectormap -->
-    {!! Html::script('adminlte/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js') !!}
-    {!! Html::script('adminlte/plugins/jvectormap/jquery-jvectormap-world-mill-en.js') !!}
+    {!! Html::script('lte/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js') !!}
+    {!! Html::script('lte/plugins/jvectormap/jquery-jvectormap-world-mill-en.js') !!}
     <!-- jQuery Knob Chart -->
-    {!! Html::script('adminlte/bower_components/jquery-knob/dist/jquery.knob.min.js') !!}
+    {!! Html::script('lte/bower_components/jquery-knob/dist/jquery.knob.min.js') !!}
     <!-- daterangepicker -->
-    {!! Html::script('adminlte/bower_components/moment/min/moment.min.js') !!}
-    {!! Html::script('adminlte/bower_components/bootstrap-daterangepicker/daterangepicker.js') !!}
+    {!! Html::script('lte/bower_components/moment/min/moment.min.js') !!}
+    {!! Html::script('lte/bower_components/bootstrap-daterangepicker/daterangepicker.js') !!}
     <!-- datepicker -->
-    {!! Html::script('adminlte/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js') !!}
+    {!! Html::script('lte/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js') !!}
     <!-- Bootstrap WYSIHTML5 -->
-    {!! Html::script('adminlte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js') !!}
+    {!! Html::script('lte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js') !!}
     <!-- Slimscroll -->
-    {!! Html::script('adminlte/bower_components/jquery-slimscroll/jquery.slimscroll.min.js') !!}
+    {!! Html::script('lte/bower_components/jquery-slimscroll/jquery.slimscroll.min.js') !!}
     <!-- FastClick -->
-    {!! Html::script('adminlte/bower_components/fastclick/lib/fastclick.js') !!}
+    {!! Html::script('lte/bower_components/fastclick/lib/fastclick.js') !!}
     <!-- AdminLTE App -->
-    {!! Html::script('adminlte/dist/js/adminlte.min.js') !!}
+    {!! Html::script('lte/dist/js/adminlte.min.js') !!}
     <!-- AdminLTE for demo purposes -->
-    {!! Html::script('adminlte/dist/js/demo.js') !!}
+    {!! Html::script('lte/dist/js/demo.js') !!}
     {!! Html::script('js/jquery-confirm.min.js') !!}
     @yield('scripts')
 </head>
